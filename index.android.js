50476316d9785a106a592ba761386f7300b6f9dc
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2017-05-07 00:23:05 -0400

    Initial commit for android application.

--- index.android.js
+++ index.android.js
@@ -1,53 +1,23 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * A simple four function calculator used to learn the basics
+ * of React Native
+ * @Author: Richard Zeng
  */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    View,
+    Text
 } from 'react-native';
+import Calculator from './Calculator';
 
-export default class calculator extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+// This is the main method for ios platform.
+class main extends Component{
+    render(){
+        return(
+            <Calculator/>
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('calculator', () => calculator);
+AppRegistry.registerComponent('calculator', () => main);
